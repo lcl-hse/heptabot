d944c3c4cf269572b46360a5606f45091a716c95
isikus <isikus@github.com>
2021-03-03 21:37:55 +0300

    Amended startup script

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 echo "heptabot is starting and will be ready in 75 seconds"
+tryenv=`conda activate heptabot`
+if [[ $tryenv != "" ]]
+then
+    source ~/mambaforge/etc/profile.d/conda.sh
+    conda activate heptabot
+fi
 pyro4-ns &
 sleep 5; python models.py &
 sleep 70; python main.py &
